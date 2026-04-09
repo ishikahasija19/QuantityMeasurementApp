ae3bcb0157a712382d9f02c6e0749910301f64e4
ishikahasija19 <ishikahasija00@gmail.com>
2026-04-09 21:21:10 +0530

    UC8

--- src/main/java/QuantityMeasurementApp/MeasurementApplication.java
+++ src/main/java/QuantityMeasurementApp/MeasurementApplication.java
@@ -2,7 +2,6 @@ package QuantityMeasurementApp;
 
 import com.quantity.measurement.enums.LengthUnit;
 import com.quantity.measurement.model.QuantityLength;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,20 +9,9 @@ public class MeasurementApplication {
 
 	public static void main(String[] args) {
 
+		QuantityLength q1 = new QuantityLength(2.0, LengthUnit.CENTIMETERS);
+		QuantityLength q2 = new QuantityLength(2.0, LengthUnit.YARDS);
 
-		SpringApplication.run(MeasurementApplication.class, args);
-
-
-		double value1 = 1.0;
-		LengthUnit unit1 = LengthUnit.FEET;
-
-		double value3 = 12.0;
-		LengthUnit unit2 = LengthUnit.INCH;
-
-		QuantityLength quantityLength = new QuantityLength(value1,unit1);
-		QuantityLength quantityLength1 = new QuantityLength(value3, unit2);
-
+		System.out.println(q1.equals(q2));
 	}
-
-
 }
--- src/main/java/com/quantity/measurement/enums/LengthUnit.java
+++ src/main/java/com/quantity/measurement/enums/LengthUnit.java
@@ -3,34 +3,27 @@ package com.quantity.measurement.enums;
 public enum LengthUnit {
 
     FEET(1.0),
-
-    INCH(1.0/12),
-
+    INCH(1.0 / 12),
     YARDS(3.0),
-
-    CENTIMETERS(0.0328084);
-
-
+    CENTIMETERS(1.0 / 13.48);
 
     private final double toFeetFactor;
 
-
-
     LengthUnit(double toFeetFactor) {
-
         this.toFeetFactor = toFeetFactor;
-
     }
 
-
-
-    public double toFeet(double value) {
-
+    public double convertToBaseUnit(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
         return value * toFeetFactor;
-
     }
 
-    public double fromFeet(double value) {
-        return value/toFeetFactor;
+    public double convertFromBaseUnit(double value) {
+        if (!Double.isFinite(value)) {
+            throw new IllegalArgumentException("Invalid value");
+        }
+        return value / toFeetFactor;
     }
 }
